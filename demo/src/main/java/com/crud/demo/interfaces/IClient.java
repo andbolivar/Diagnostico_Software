858e51a09d0699ae4abab8f9f0f876ef45790792
andbolivar <and.bolivar@gmail.com>
2020-03-04 19:08:03 -0400

    Update

--- demo/src/main/java/com/crud/demo/interfaces/IClient.java
+++
@@ -1,9 +0,0 @@
-package com.crud.demo.interfaces;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.crud.demo.modelo.Client;
-
-public interface IClient extends CrudRepository<Client, Integer>{
-
-}
